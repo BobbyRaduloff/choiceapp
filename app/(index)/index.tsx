b1c77ba90a0cc07193b66509d9e732b5e8e3ad4f
Boris Radulov <sc20br@leeds.ac.uk>
2024-03-06 23:13:39 +0200

    removed tabs for now

---
+++ app/(index)/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="w-full min-h-full flex items-center justify-center">
+      <Text className="text-2xl">Hello, world!</Text>
+    </View>
+  );
+}
--- app/(tabs)/index.tsx
+++
@@ -1,9 +0,0 @@
-import { Text, View } from "react-native";
-
-export default function TabOneScreen() {
-  return (
-    <View className="flex flex-col items-center justify-center">
-      <Text>Tab One</Text>
-    </View>
-  );
-}
